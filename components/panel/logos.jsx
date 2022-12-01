9ff321c1c62a01a8915c6dfd475b1e35a66005c0
Ahmed Amawi <ahmed@atgpharma.com>
2022-11-30 17:18:59 -0800

    updated jro logo

--- components/panel/logos.jsx
+++ components/panel/logos.jsx
@@ -1,7 +1,11 @@
 export default function Logos() {
   return (
     <div className="bg-black flex flex-row  justify-center items-center py-3 gap-4">
-      <img className="h-10 w-auto" src="/JRO.png" />
+                  <a
+              href="https://jackrabbitops.com/"
+              target="_blank"
+              rel="noreferrer"
+            ><img className="h-10 w-auto" src="/JRO.png" /></a>
       <p className="text-gray-100 text-sm">+ more</p>
     </div>
   );
